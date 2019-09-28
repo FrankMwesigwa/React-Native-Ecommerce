995f18b1d230a2db4741185df091cbd69557058c
Frank Mwesigwa <frank.mwesigwa1@gmail.com>
2019-09-28 12:37:26 +0300

    add touchableopacity to details screen

--- src/navigation/ShopNavigator.js
+++ src/navigation/ShopNavigator.js
@@ -1,21 +1,23 @@
-import {createStackNavigator, createAppContainer} from 'react-navigation';
-import {Platform} from 'react-native';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+import { Platform } from 'react-native';
 
 import ProductsOverviewScreen from '../screens/shop/ProductsOverviewScreen';
+import ProductDetailScreen from '../screens/shop/ProductDetailScreen';
 import Colors from '../constants/Colors';
 
 const ProductsNavigator = createStackNavigator(
   {
     ProductsOverview: ProductsOverviewScreen,
+    ProductDetail: ProductDetailScreen
   },
   {
     defaultNavigationOptions: {
       headerStyle: {
-        backgroundColor: Platform.OS === 'android' ? Colors.primary : '',
+        backgroundColor: Platform.OS === 'android' ? Colors.primary : ''
       },
-      headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primary,
-    },
-  },
+      headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primary
+    }
+  }
 );
 
 export default createAppContainer(ProductsNavigator);
